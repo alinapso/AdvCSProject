019a0f7d74e43ee350f893bf8dc9cf06f8a12885
Sloth <rodinhatokay1998@gmail.com>
2020-09-03 17:30:14 +0300

    fixed formatting using eslint

--- Models/User.js
+++ Models/User.js
@@ -8,39 +8,39 @@ class User extends Model {}
 
 User.init(
 
-  {
-    // attributes
-    id: {
-      type: Sequelize.UUID,
-      allowNull: false,
-      primaryKey: true,
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    password: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    firstName: {
-      type: Sequelize.STRING,
-      allowNull: true,
-    },
-    familyName: {
-      type: Sequelize.STRING,
-      allowNull: true,
-    },
-    groupID: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    modelName: "user",
-    // options
-  }
+	{
+		// attributes
+		id: {
+			type: Sequelize.UUID,
+			allowNull: false,
+			primaryKey: true,
+		},
+		email: {
+			type: Sequelize.STRING,
+			allowNull: false,
+		},
+		password: {
+			type: Sequelize.STRING,
+			allowNull: false,
+		},
+		firstName: {
+			type: Sequelize.STRING,
+			allowNull: true,
+		},
+		familyName: {
+			type: Sequelize.STRING,
+			allowNull: true,
+		},
+		groupID: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		modelName: "user",
+		// options
+	}
 
 );
 
