5aa1231181ecfd20ea34f319772f6a601961192e
Mosa-T <48522718+Mosa-T@users.noreply.github.com>
2020-09-04 23:19:43 +0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ var helper = require("./helper.js");
 var path = require("path");
 const { adminOnly } = require("./helper.js");
 const User = require("../Models/User");
+const JobType = require("../Models/JobType");
 
 var appDir = path.dirname(require.main.filename) + "/public";
 
@@ -40,5 +41,40 @@ router.post("/admin-workers", helper.adminOnly, async (req, res) => {
 		res.redirect("/");
 	}
 });
+
+
+// router.get("/admin", helper.adminOnly, (req, res) => {
+// 	console.log("HEY IM HERE1");
+// 	return res.sendFile(appDir + "/admin-workers.html");
+// });
+
+router.post('/admin',helper.adminOnly,async (req,res)=>{
+	try {
+		console.log(req.body);
+		if (!req.body.jobName) {
+			return res.sendStatus(400);
+		}
+		try {
+			const jobType2 = JobType.build({
+				id: Date.now().toString(),
+				jobType: req.body.jobName,
+				workerList: null,
+			});
+			console.log(jobType2);
+			await jobType2.save();
+			// res.redirect("/login");
+			console.log("SUCCSEFULLY ADDED NEW JOB ");
+
+			return res.sendStatus(200);
+			//next();
+		} catch (err) {
+			console.log("ERROR ADDING NEW JOB:", err);
+			return res.sendStatus(400);
+		}
+	} catch {
+		res.redirect("/");
+	}
+});
+
 
 module.exports = router;
