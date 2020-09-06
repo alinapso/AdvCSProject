6e69a16fb165f6afc920d6c7a664fb46564a3d8a
Sloth <rodinhatokay1998@gmail.com>
2020-09-06 16:07:16 +0300

    Update data.js

--- routes/data.js
+++ routes/data.js
@@ -9,20 +9,20 @@ var sequelize = require("../sql.js");
 const { Op, QueryTypes } = require("sequelize");
 
 router.get("/groups", helper.checkAuthenticated, async (req, res) => {
-  const result = await Group.findAll({ where: { id: { [Op.gt]: 1 } } });
-  return res.json(result);
+	const result = await Group.findAll({ where: { id: { [Op.gt]: 1 } } });
+	return res.json(result);
 });
 
 router.get("/client/tasks", helper.checkAuthenticated, async (req, res) => {
-  const result = await Task.findAll({ where: { clientID: req.user.id } });
-  return res.json(result);
+	const result = await Task.findAll({ where: { clientID: req.user.id } });
+	return res.json(result);
 });
 router.get("/users/getall", helper.adminOnly, async (req, res) => {
-  const result = await sequelize.query(
-    "SELECT users.id,users.email,users.firstName,users.familyName,groups.name as groupname from users inner join groups on users.groupID = groups.name;",
-    { type: QueryTypes.SELECT }
-  );
-  console.log(result);
-  return res.json(result);
+	const result = await sequelize.query(
+		"SELECT users.id,users.email,users.firstName,users.familyName,groups.name as groupname from users inner join groups on users.groupID = groups.name;",
+		{ type: QueryTypes.SELECT }
+	);
+	console.log(result);
+	return res.json(result);
 });
 module.exports = router;
