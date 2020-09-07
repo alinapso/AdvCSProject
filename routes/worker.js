c3472018280e9818035393fbbebba371a865d9c8
Mosa-T <48522718+Mosa-T@users.noreply.github.com>
2020-09-08 01:15:47 +0300

    Update worker.js

--- routes/worker.js
+++ routes/worker.js
@@ -3,6 +3,7 @@ const router = express.Router();
 var helper = require("./helper.js");
 // const passport = require("passport");
 var path = require("path");
+const Tasks = require("../Models/Tasks.js");
 var appDir = path.dirname(require.main.filename) + "/public";
 
 // router.get("/worker", helper.clientOnly, (req, res) => {
@@ -17,4 +18,26 @@ router.get("/worker-profile", helper.workerOnly, (req, res) => {
 	res.sendFile(appDir + "/worker-profile.html");
 });
 
+
+
+router.post(
+    "/worker-tasks/update-task",
+    helper.workerOnly,
+    async (req, res) => {
+        try {
+            const result = await Tasks.findByPk(req.body.taskID);
+			result.workerID = req.user.id;
+            result.status = req.body.status;
+            result.save();
+            return res.sendStatus(200);
+        } catch (err) {
+            return res.status(400).send({ msg: err });
+        }
+    }
+);
+
+
+
+
 module.exports = router;
+
