9dcca0f830f1adc5c4bbc9d2589339e79495182b
Mosa-T <48522718+Mosa-T@users.noreply.github.com>
2020-09-25 09:11:11 +0300

    Update tasks.js

--- public/admin/js/tasks.js
+++ public/admin/js/tasks.js
@@ -10,16 +10,22 @@ fetch("/api/tasks/")
 
     var list = document.getElementById("tasksList");
     for (let i = 0; i < data.length; i++) {
-      list.innerHTML +=
-        "<li class='col-md-12 col-sm-12'>" +
-        "  <div class='time task' id='task.id'>" +
-        ` <div class='text' >Client ID: ${data[i].clientID} <br>CreatedAt: ${data[i].createdAt} </div>` +
-        `<div style="height:100px;width:180px;border:1px solid #4e4e4e;font:16px Arial, Serif;overflow:auto;">${data[i].details}</div>` +
-        " </div>" +
-        "</li>";
-    }
+		list.innerHTML +=
+			"<li class='col-md-12 col-sm-12'>" +
+			"  <div class='time task' id='task.id'>" +
+			` <div class='text' >Client ID: ${data[i].clientID} <br>CreatedAt: ${data[i].createdAt} <br> Address: ${data[i].address} <br>Status: ${getStatus(data[i])}  </div>` +
+			`<div style="height:100px;width:180px;border:1px solid #4e4e4e;font:16px Arial, Serif;overflow:auto;">${data[i].details}</div>`+
+			" </div>" +
+			"</li>";
+	}
   })
   .catch((err) => {
     console.log(err);
     // Do something for an error here
   });
+
+  function getStatus(data) {
+	if (data.status === 0) return "Waiting to be accepted";
+	if (data.status === 1) return `Accepted by a workerID: ${data.workerID}`;
+	if(data.status === 2) return `Job done by a workerID: ${data.workerID}`;
+}
